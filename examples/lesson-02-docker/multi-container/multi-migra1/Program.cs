7a1ca93236b256083f3b2600125236a247312b5c
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-01-05 08:22:10 +0100

    Migration stuff

---
+++ examples/lesson-02-docker/multi-container/multi-migra1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+      var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            //.. for test
+            Console.WriteLine(Configuration["ConnectionString"]);
