317fc555b0da5fb9317b42fdca79a1ffa189330a
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-01-05 16:07:30 +0100

    Remove unused projects

--- examples/lesson-02-docker/multi-container/multi-migra1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-      var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-
-            //.. for test
-            Console.WriteLine(Configuration["ConnectionString"]);
