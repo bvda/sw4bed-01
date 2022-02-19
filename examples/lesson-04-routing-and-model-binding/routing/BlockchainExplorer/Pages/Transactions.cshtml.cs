e0b40efc60b4186cdeb949ef4217f397c767bc49
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-02-19 15:25:21 +0100

    Add pages

---
+++ examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Pages/Transactions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlockchainExplorer.Pages
+{
+    public class TransactionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Pages/Wallets.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlockchainExplorer.Pages
+{
+    public class WalletsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
