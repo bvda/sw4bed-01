8775fcd506aef1d09713a1b38445275d45989f05
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-02-20 22:20:32 +0100

    Updated Genesis block time

--- examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
+++ examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
@@ -4,7 +4,7 @@ namespace BlockchainExplorer.Validation
 {
   public class BitcoinGenesisRangeAttribute : RangeAttribute
   {
-      public BitcoinGenesisRangeAttribute() : base(1230980400, DateTimeOffset.UtcNow.ToUnixTimeSeconds()) 
+      public BitcoinGenesisRangeAttribute() : base(1231006505, DateTimeOffset.UtcNow.ToUnixTimeSeconds()) 
       { 
       } 
   }
