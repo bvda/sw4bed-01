f75cf6292d25b644bd13ae6036b152ec710bbeb7
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-02-21 21:25:57 +0100

    Add custom validation

--- examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
+++ examples/lesson-04-routing-and-model-binding/routing/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
@@ -4,14 +4,14 @@ namespace BlockchainExplorer.Validation
 {
   public class BitcoinGenesisRangeAttribute : ValidationAttribute 
   {
-      public BitcoinGenesisRangeAttribute()
-      { 
-      } 
+    public BitcoinGenesisRangeAttribute()
+    { 
+    } 
 
-      public override bool IsValid(object value) 
-      {
-        var dt = (int)value;
-        return dt >= 1231006505 && dt <= DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-      }
+    public override bool IsValid(object value) 
+    {
+      var dt = (int)value;
+      return dt >= 1231006505 && dt <= DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    }
   }
 }
