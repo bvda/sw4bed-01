995efdbb5178ac5cca01eed8984b925a72c1c4bd
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-03-12 12:05:01 +0100

    Add dev theme

--- examples/lesson-07-configuration/WebAPIConfiguration/Options/AppShellOptions.cs
+++ examples/lesson-07-configuration/WebAPIConfiguration/Options/AppShellOptions.cs
@@ -7,10 +7,10 @@ public class AppShellOptions {
 
   public class ColorsOption {
     public string Primary { get; set; } = String.Empty;
+    public string PrimaryLight { get; set; } = String.Empty;
+    public string PrimaryDark { get; set; } = String.Empty;
     public string Secondary { get; set; } = String.Empty;
-    public string Tertiary { get; set; } = String.Empty;
-    public string PrimaryContainer { get; set; } = String.Empty;
-    public string SecondaryContainer { get; set; } = String.Empty;
-    public string TertiaryContainer { get; set; } = String.Empty;
+    public string SecondaryLight { get; set; } = String.Empty;
+    public string SecondaryDark { get; set; } = String.Empty;
   }
 }
