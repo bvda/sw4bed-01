3b769f490b673e47f7b3650f39ea3c72288d04d8
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-03-14 15:57:18 +0100

    Fix nullable type for model and db context

--- examples/lesson-07-configuration/WebAPIConfiguration/Data/NetLogContext.cs
+++ examples/lesson-07-configuration/WebAPIConfiguration/Data/NetLogContext.cs
@@ -6,7 +6,7 @@ using WebAPIConfiguration.Model;
 namespace WebAPIConfiguration.Data;
 
 public class NetLogContext : DbContext {
-  public DbSet<NetLog> NetLogs { get; set; }
+  public DbSet<NetLog> NetLogs => Set<NetLog>();
 
   public NetLogContext(DbContextOptions<NetLogContext> options) : base(options) { }
 }
