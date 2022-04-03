1b6ca8161e3b0ede9720ed1e6c49915fe59e2ada
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-04-03 11:32:56 +0200

    Add example code

---
+++ examples/lesson-10/bundling-and-minification/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿function myFunc() {
+  // this function doesn't really do anything,
+  // it's just here so that we can show off minifying!
+  function innerFunctionToAddTwoNumbers(thefirstnumber, theSecondNumber) {
+    // i'm nested inside myFunc
+    return thefirstnumber + theSecondNumber;
+  }
+  var shouldAddNumbers = true;
+  var totalOfAllTheNumbers = 0;
+  if (shouldAddNumbers == true) {
+    for (var index = 0; i < 10; i++) {
+      totalOfAllTheNumbers = innerFunctionToAddTwoNumbers(
+        totalOfAllTheNumbers,
+        index
+      );
+    }
+  }
+  return totalOfAllTheNumbers;
+}
