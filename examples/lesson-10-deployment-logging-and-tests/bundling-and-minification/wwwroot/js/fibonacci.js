4692cd1aa14f1268394e8c0be46d0a11f50ce52c
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-04-04 10:58:01 +0200

    Add example code

---
+++ examples/lesson-10-deployment-logging-and-tests/bundling-and-minification/wwwroot/js/fibonacci.js
@@ -0,0 +1,29 @@
+﻿// https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+
+function memo(num, memo) {
+  memo = memo || {};
+
+  if (memo[num]) return memo[num];
+  if (num <= 1) return 1;
+
+  return memo[num] = memo(num - 1, memo) + memo(num - 2, memo);
+}
+
+function loop(num){
+  var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+function recursion(num) {
+  if (num <= 1) return 1;
+
+  return recursion(num - 1) + recursion(num - 2);
+}
