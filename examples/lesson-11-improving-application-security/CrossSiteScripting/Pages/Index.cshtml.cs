f8e13b6a6ab6b094cb24608ca297d4badb1c28ed
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-04-14 13:02:05 +0200

    Add XSS example

---
+++ examples/lesson-11-improving-application-security/CrossSiteScripting/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrossSiteScripting.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    [BindProperty]
+    public string FormInput { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    //<script>alert("Wow!")</script> 
+    public void OnPost() {
+    }
+}
