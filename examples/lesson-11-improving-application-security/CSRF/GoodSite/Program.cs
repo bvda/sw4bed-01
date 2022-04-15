0e1166623ea6ce6e50df236fdb85b85253100e59
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-04-15 12:22:47 +0200

    Remove unused imports

--- examples/lesson-11-improving-application-security/CSRF/GoodSite/Program.cs
+++ examples/lesson-11-improving-application-security/CSRF/GoodSite/Program.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using GoodSite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
