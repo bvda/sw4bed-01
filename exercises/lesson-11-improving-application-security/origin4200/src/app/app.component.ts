f103821626262585322f2ff290b36dc7393e7cd8
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-04-19 09:05:28 +0200

    Add buttons

--- exercises/lesson-11-improving-application-security/origin4200/src/app/app.component.ts
+++ exercises/lesson-11-improving-application-security/origin4200/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'origin4200';
+
+  constructor(private http: HttpClient) { }
+
+  onClick() {
+    this.http.post('https://localhost:5001', {
+      message: "Hello from 'Origin4200'"
+    }).subscribe(console.log)
+  }  
 }
--- exercises/lesson-11-improving-application-security/origin4300/src/app/app.component.ts
+++ exercises/lesson-11-improving-application-security/origin4300/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'origin4300';
+  
+  constructor(private http: HttpClient) { }
+
+  onClick() {
+    this.http.post('https://localhost:5001', {
+      message: "Hello from 'Origin4300'"
+    }).subscribe(console.log)
+  }
 }
