9ea1150adc2b2f330dd0bc4e9881bc0dde647de0
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-04-25 15:45:51 +0200

    Add slides

--- examples/lesson-12-persisting-data-with-mongo-db/mtg-collection/Services/CardService.cs
+++ examples/lesson-12-persisting-data-with-mongo-db/mtg-collection/Services/CardService.cs
@@ -9,6 +9,7 @@ public class CardService {
 
   private readonly ILogger<CardService> _logger;
   private readonly IMongoCollection<Card> _collection;
+
   public CardService(ILogger<CardService> logger, MongoService service) {
     _collection = service.Client.GetDatabase("mtg").GetCollection<Card>("cards");
     _logger = logger;
@@ -17,15 +18,19 @@ public class CardService {
   public async Task<IList<Card>> Search(string? name = null, string? type = null, string? set = null) {
     var builder = Builders<Card>.Filter;
     var filter = builder.Empty;
+
     if(name?.Length > 0) {
       filter &= builder.Regex(x => x.Name, new BsonRegularExpression($"/{name}/i"));
     }
+
     if(type?.Length > 0) {
       filter &= builder.Regex(x => x.Type, new BsonRegularExpression($"/{type}/i"));
     }
+
     if(set?.Length > 0) {
       filter &= builder.Eq(x => x.SetCode, set);
     }
+    
     return await _collection.Find(filter).ToListAsync();
   }
 }
