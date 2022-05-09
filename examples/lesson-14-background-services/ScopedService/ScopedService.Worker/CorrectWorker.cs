4cc2032476b29e6684e12551dc33130cbfeeaa59
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-05-09 15:22:33 +0200

    Add fix worker

--- examples/lesson-14-background-services/ScopedService/ScopedService.Worker/CorrectWorker.cs
+++ examples/lesson-14-background-services/ScopedService/ScopedService.Worker/CorrectWorker.cs
@@ -6,12 +6,10 @@ public class CorrectWorker : BackgroundService
 {
   private readonly ILogger<CorrectWorker> _logger;
   private readonly ICurrencyServiceFactory _factory;
-  private readonly IServiceProvider _provider;
 
-  public CorrectWorker(ILogger<CorrectWorker> logger, IServiceProvider provider, ICurrencyServiceFactory factory)
+  public CorrectWorker(ILogger<CorrectWorker> logger,ICurrencyServiceFactory factory)
   {
     _logger = logger;
-    _provider = provider;
     _factory = factory;
   }
 
@@ -19,16 +17,8 @@ public class CorrectWorker : BackgroundService
   {
     while (!stoppingToken.IsCancellationRequested)
     {
-      using (var scope = _provider.CreateScope())
-      {
-        var api = _factory.GetCurrencyService();
-        var result = await api.GetRandomCurrency();
-        if (result is not null) {
-          var db = scope.ServiceProvider.GetRequiredService<ExchangeDbContext>();
-          await db.AddAsync(result);
-          await db.SaveChangesAsync();
-        }
-      }
+      var api = _factory.GetCurrencyService();
+      var result = await api.GetRandomCurrency();
       _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
       await Task.Delay(1000, stoppingToken);
     }
