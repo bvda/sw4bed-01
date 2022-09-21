b08daf4feb2378c2b1fae83f1b624551e8ec5775
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-09-21 08:58:09 +0200

    Clean up code

--- examples/lesson-04-routing-and-model-binding/routing-and-model-binding/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
+++ examples/lesson-04-routing-and-model-binding/routing-and-model-binding/BlockchainExplorer/Validation/BitcoinGenesisValidator.cs
@@ -8,9 +8,12 @@ namespace BlockchainExplorer.Validation
     { 
     } 
 
-    public override bool IsValid(object value) 
+    public override bool IsValid(object? value = null) 
     {
-      var dt = (int)value;
+      var dt = 0;
+      if(value is not null) {
+        dt = (int)value;
+      }
       return dt >= 1231006505 && dt <= DateTimeOffset.UtcNow.ToUnixTimeSeconds();
     }
   }
